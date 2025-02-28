1cb6d7d6c6e66c8497ab55db57c34ae9cce81b13
Nick Monaco <neomanslayer@gmail.com>
2025-02-28 01:56:50 -0500

    added thenickmonaco.com/aphorisms

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -19,7 +19,7 @@ func main() {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// Your home page or any response for htmx interaction
-	tmpl, err := template.ParseFiles("htmx/html/index.html")
+	tmpl, err := template.ParseFiles("htmx/html/home.html")
 	if err != nil {
 		http.Error(w, "Error loading template", http.StatusInternalServerError)
 		return
